ab14eff889de69b8a2c3110fc495416ce57ab4ad
lcs912 <1757441045@qq.com>
2026-06-06 20:58:08 +0800

    Delete app/app directory

--- app/app/app/app/app/rebalance.py
+++
@@ -1,8 +0,0 @@
-def rebalance(current, target):
-
-    actions = {}
-
-    for k in target:
-        actions[k] = round(target[k] - current.get(k,0), 3)
-
-    return actions
--- app/app/app/data.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-
-def get_data():
-
-    df = yf.download(["SPY","QQQ","TLT","GLD"], period="1y")["Close"]
-
-    return df.dropna()
